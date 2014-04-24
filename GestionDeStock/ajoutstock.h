9da12b38f0efb2b646e7dc71b42959a85766f39b
Pawamoy <gein@hotmail.fr>
2014-04-24 21:49:31 +0200

    wip all

--- GestionDeStock/ajoutstock.h
+++ GestionDeStock/ajoutstock.h
@@ -3,6 +3,12 @@
 
 #include <QDialog>
 
+#include "stock.h"
+#include "comboboxcategory.h"
+#include "comboboxcolor.h"
+#include "comboboxsize.h"
+#include "comboboxtype.h"
+
 namespace Ui {
 class ajoutStock;
 }
@@ -12,11 +18,16 @@ class ajoutStock : public QDialog
     Q_OBJECT
     
 public:
-    explicit ajoutStock(QWidget *parent = 0);
+    explicit ajoutStock(QWidget *parent = 0, Stock* s = NULL);
     ~ajoutStock();
     
 private:
     Ui::ajoutStock *ui;
+    Stock* stock;
+
+private slots:
+    void on_annuler_clicked();
+    void on_valider_clicked();
 };
 
 #endif // AJOUTSTOCK_H
