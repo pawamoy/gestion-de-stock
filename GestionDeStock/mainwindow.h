85b111f91d6f4c3f928d8a902e8d4041029ed19f
Pawamoy <gein@hotmail.fr>
2014-04-24 15:48:29 +0200

    wip tableau ro et rw

--- GestionDeStock/mainwindow.h
+++ GestionDeStock/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+
+#include "comboboxcolor.h"
+#include "comboboxcategory.h"
+#include "comboboxtype.h"
+#include "comboboxsize.h"
+#include "enregistrement.h"
+#include "stock.h"
 
 namespace Ui {
 class MainWindow;
@@ -14,12 +22,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+    void OpenStock(const char* s);
+    void DeleteStock();
+    void FillTableRO();
+    void FillTableRW();
+    void InsertRowRO(int r, StockArticle* sa);
+    void InsertRowRW(int r, StockArticle* sa);
     
 private slots:
     void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
+    Stock* stock;
 };
 
 #endif // MAINWINDOW_H
